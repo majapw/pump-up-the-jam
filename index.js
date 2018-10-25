8633b78e9d2e85c132f8ffcbef945559f7835731
Maja Wichrowska <majapw@gmail.com>
2018-10-25 03:35:12 -0400

    Add explanatory modal

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ function main() {
   const quoteText = document.querySelector('.quote');
   const quoteAuthor = document.querySelector('.author');
   const gimmeMore = document.querySelector('.gimme-more');
+  const whatIsThis = document.querySelector('.what-is-this');
+  const close = document.querySelector('.close');
+  const body = document.querySelector('body');
+  const modalContainer = document.querySelector('.modal-container');
 
   const songTitle = document.querySelector('.song-title');
   const songEmbed = document.querySelector('.song-embed');
 
   const { quotes, songs} = inspiration;
+
+  const gimme = ['Give me another', 'Gimme another one', 'I need more inspiration', 'Pump me up!', 'I\'m not tired!', 'Hit me'];
 
   let currentQuoteIndex = Math.floor(Math.random() * quotes.length);
   let currentSongIndex = Math.floor(Math.random() * songs.length);
@@ -30,6 +36,19 @@ function main() {
     currentSongIndex = (currentQuoteIndex + 1) % songs.length;
     populateQuote();
     populateSong();
+
+    const randomGimme = gimme[Math.floor(Math.random() * gimme.length)];
+    gimmeMore.innerText = randomGimme;
+  });
+
+  whatIsThis.addEventListener('click', () => {
+    body.classList.add('freeze');
+    modalContainer.classList.remove('hide');
+  });
+
+  close.addEventListener('click', () => {
+    body.classList.remove('freeze');
+    modalContainer.classList.add('hide');
   });
 }
 
